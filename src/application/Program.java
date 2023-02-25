1c609f0be7d27107542df5729abc38e4d8ab923b
Richard Guedes <richard.silva46@fatec.sp.gov.br>
2023-02-25 15:41:12 -0300

    Seller Entity Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,19 +1,25 @@
 package application;
 
 import entities.Department;
+import entities.Seller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
+
 public class Program {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
         Scanner sc = new Scanner(System.in);
-
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         List<Department> departments = new ArrayList<>();
+        List<Seller> sellers = new ArrayList<>();
 
-        System.out.print("Enter the quantity of departments: ");
+        System.out.print("Enter the quantity of Sellers: ");
         int quantity = sc.nextInt();
 
         System.out.println();
@@ -21,14 +27,27 @@ public class Program {
         sc.nextLine();
         for (int i =0; i < quantity; i++) {
 
-            System.out.print("Enter the name of the department for ID " + (i + 1) + ": ");
+            System.out.print("Enter the name of Seller for ID " + (i + 1) + ": ");
             String name = sc.nextLine();
-            departments.add(new Department((i + 1), name));
+            System.out.print("Enter the email for " + name + ": ");
+            String email = sc.nextLine();
+            System.out.print("Enter BirthDate for " + name + ": ");
+            Date birthDate = sdf.parse(sc.nextLine());
+            System.out.print("Enter the BaseSalary for " + name + ": ");
+            double baseSalary = sc.nextDouble();
+            System.out.println("Enter the ID Department for " + name + ": ");
+            int IdepartmentId = sc.nextInt();
+            sc.nextLine();
+            System.out.println("Enter the name department for " + name + ": ");
+            String departmentName = sc.nextLine();
+
+            sellers.add(new Seller((i + 1), name, email, birthDate, baseSalary, (new Department(IdepartmentId, departmentName))));
+
         }
 
         System.out.println();
         System.out.println("==========================");
-        departments.forEach(System.out::println);
+        sellers.forEach(System.out::println);
 
 
     }
---
+++ src/entities/Seller.java
@@ -0,0 +1,113 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class Seller implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private Integer id;
+    private String name;
+    private String email;
+    private Date birthDate;
+    private Double baseSalary;
+    private Department department;
+
+    public Seller() {
+    }
+
+    public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.baseSalary = baseSalary;
+        this.department = department;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Seller seller = (Seller) o;
+
+        if (!Objects.equals(id, seller.id)) return false;
+        if (!Objects.equals(name, seller.name)) return false;
+        if (!Objects.equals(email, seller.email)) return false;
+        if (!Objects.equals(birthDate, seller.birthDate)) return false;
+        if (!Objects.equals(baseSalary, seller.baseSalary)) return false;
+        return Objects.equals(department, seller.department);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (birthDate != null ? birthDate.hashCode() : 0);
+        result = 31 * result + (baseSalary != null ? baseSalary.hashCode() : 0);
+        result = 31 * result + (department != null ? department.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Seller{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", birthDate=" + birthDate +
+                ", baseSalary=" + baseSalary +
+                ", department=" + department +
+                '}';
+    }
+}
